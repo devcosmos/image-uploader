8ddde8dc87e4c0101052b62b4c0879cbbcbc5807
cosmos <va-litv@yandex.ru>
2023-09-29 14:59:09 +0300

    style: update logo

--- index.php
+++ index.php
@@ -12,7 +12,9 @@
       <div class="row justify-content-center">
         <div class="col-lg-6 py-5 my-5">
           <div class="text-center">
-            <img class="d-block mx-auto mb-4" src="https://mai.ru/press/brand/download/Inverse/RU/Inverse.svg" height="60">
+            <a href="/">
+              <img class="d-block mx-auto mb-4" src="https://mai.ru/bitrix/templates/mai23_dev/php/label/logo.svg" height="60">
+            </a>
             <h1 class="display-6 fw-bold text-body-emphasis">Загрузка изображения</h1>
             <p class="lead mb-5">Сервис для загрузки изображений на files.mai.ru.</p>
           </div>
