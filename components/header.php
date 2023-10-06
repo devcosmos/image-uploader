cc582321f90198c3bf9fc2355dc10c691044d076
cosmos <va-litv@yandex.ru>
2023-10-06 17:26:16 +0300

    style: update title and subtitle

--- components/header.php
+++ components/header.php
@@ -3,5 +3,5 @@
     <img class="d-block mx-auto mb-4" src="https://mai.ru/bitrix/templates/mai23_dev/php/label/logo.svg" height="60">
   </a>
   <h1 class="display-6 fw-bold text-body-emphasis">Загрузка изображения</h1>
-  <p class="lead mb-5">Сервис для загрузки изображений на files.mai.ru.</p>
+  <p class="lead mb-5">Сервис для загрузки изображений для рассылки</p>
 </div>
